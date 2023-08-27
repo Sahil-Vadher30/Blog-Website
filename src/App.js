ed3c86440fcb08e8da0a5e705b2b4b28ad70b0bc
Sahil Vadher <sahilvadher1977@gmail.com>
2023-08-27 17:50:31 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,20 +6,55 @@ import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 
 function App() {
+
+  const user = true;
   return (
     
-    <>
+    <Router>
         <TopBar/>
+
+        <Routes>
+          <Route 
+            path="/" 
+            element={<Home/>} />
+          <Route 
+            path="/register" 
+            element={user? <Home/> : <Register/>} />
+          <Route 
+            path="/login" 
+            element={<Login/>} />
+          <Route 
+            path="/write" 
+            element={user ? <Write/> : <Register/>} />
+          {/* <Route 
+            path="/settings" 
+            element={user ? <Settings/> : <Register/>} /> */}
+          <Route 
+            path="/post/:postId" 
+            element={<Single/>} />
+
+        </Routes>
+
+
+
+
+        {/* ----------------------------------------------------- */}
         {/* <Home/> */}
         {/* <Single/> */}
         {/* <Write/> */}
         {/* <Login/> */}
-        <Register/>
-    </>
+        {/* <Register/> */}
+    </Router>
 
   );
 }
